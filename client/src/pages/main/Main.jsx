cee64e8ffb14b6c646905864da14e49aa9c80416
likeprograming1 <hsj2216@naver.com>
2022-11-11 16:35:58 +0900

    eslint설정

---
+++ client/src/pages/main/Main.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Maincontainer = styled.main`
+  width: 100%;
+  height: 100vh;
+`;
+
+const Main = () => {
+  return <Maincontainer></Maincontainer>;
+};
+
+export default Main;
