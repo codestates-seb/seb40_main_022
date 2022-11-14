ef73b16aded99a4508e8cc1fc0bc9b8d502d9167
Gojeonghun <apple@appleui-MacBookPro.local>
2022-11-14 10:21:12 +0900

    Qna 목업

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Main from './pages/main/Main';
+import QnaList from './pages/Qna/QnaList';
+import QnaAsk from './pages/Qna/QnaAsk';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route path="/" element={<QnaList />} />
+      <Route path="/qnaask" element={<QnaAsk />} />
     </Routes>
   );
 }
+
+// Pull하기 전에 QnaList 를 Main으로 바꾸고 임포트지우기
 
 export default App;
