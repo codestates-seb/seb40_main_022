c55ee2378bc12c7797106c7e38719ba9d8311024
Gojeonghun <apple@appleui-MacBookPro.local>
2022-11-14 10:27:37 +0900

    pull 전 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import QnaList from './pages/Qna/QnaList';
+import Main from './pages/main/Main';
 import QnaAsk from './pages/Qna/QnaAsk';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<QnaList />} />
+      <Route path="/" element={<Main />} />
       <Route path="/qnaask" element={<QnaAsk />} />
     </Routes>
   );
 }
-
-// Pull하기 전에 QnaList 를 Main으로 바꾸고 임포트지우기
 
 export default App;
