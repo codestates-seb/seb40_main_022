851255dfe2f1c51a51722947b6ea7519ac07bd13
ko9612 <kohana735@gmail.com>
2022-11-15 18:25:18 +0900

    main 목업 1차(미완)

---
+++ client/src/components/main/TopList.jsx
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+import daily from '../../images/daily.svg';
+
+export const Top = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  height: 200px;
+  outline: none;
+  overflow-y: hidden;
+  overflow-x: hidden;
+
+  /* @media screen and (max-width: 1310px) {
+    display: none;
+  } */
+`;
+
+export const Content = styled.div`
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .circle {
+    width: 130px;
+    height: 130px;
+    border: 5px solid var(--logored);
+    border-radius: 50%;
+  }
+  > img {
+    cursor: pointer;
+    width: 110px;
+    height: 110px;
+    border-radius: 50%;
+    object-fit: cover;
+    position: absolute;
+    top: 35px;
+  }
+`;
+
+export default function TopList() {
+  return (
+    <Top>
+      <Content>
+        <div className="circle" />
+        <img src={daily} alt="daily" />
+        <span>운동인</span>
+      </Content>
+      <Content>
+        <div className="circle" />
+        <img src={daily} alt="daily" />
+        <span>운동인</span>
+      </Content>
+      <Content>
+        <div className="circle" />
+        <img src={daily} alt="daily" />
+        <span>운동인</span>
+      </Content>
+      <Content>
+        <div className="circle" />
+        <img src={daily} alt="daily" />
+        <span>운동인</span>
+      </Content>
+      <Content>
+        <div className="circle" />
+        <img src={daily} alt="daily" />
+        <span>운동인</span>
+      </Content>
+    </Top>
+  );
+}
--- client/src/pages/main/Main.jsx
+++ client/src/pages/main/Main.jsx
@@ -1,21 +1,20 @@
 import styled from 'styled-components';
-import Footer from '../../components/footer/Footer';
+// import Footer from '../../components/footer/Footer';
 import Head from '../../components/header/Header';
+import MainInside from '../../components/main/MainInside';
 
 const Maincontainer = styled.main`
   width: 100%;
-  height: 100vh;
-  > div {
-    height: 800px;
-  }
+  height: 100%;
+  background-color: var(--backcolor);
 `;
 
 function Main() {
   return (
     <Maincontainer>
       <Head />
-      <div />
-      <Footer />
+      <MainInside />
+      {/* <Footer /> */}
     </Maincontainer>
   );
 }
