a03095f37d6df3a561907b1abeac0fe602f303f4
abcodefg <a74566463@gmail.com>
2022-11-19 19:03:16 +0900

    fix : question, answer, answerComment 수정, 조회 기능 일부 수정

--- server/src/main/java/com/backend/fitchallenge/domain/answer/repository/AnswerRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package com.backend.fitchallenge.domain.answer.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnswerRepositoryCustom {
-}
--- server/src/main/java/com/backend/fitchallenge/domain/answercomment/repository/AnswerCommentRepository.java
+++ server/src/main/java/com/backend/fitchallenge/domain/answercomment/repository/AnswerCommentRepository.java
@@ -3,9 +3,10 @@ package com.backend.fitchallenge.domain.answercomment.repository;
 import com.backend.fitchallenge.domain.answercomment.entity.AnswerComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AnswerCommentRepository extends JpaRepository<AnswerComment, Long> {
 
     @Query("select ac.member.id from AnswerComment ac where ac.id = :answerCommentId")
-    Long findMemberIdByAnswerCommentId(Long answerCommentId);
+    Long findMemberIdByAnswerCommentId(@Param("answerCommentId") Long answerCommentId);
 }
