3dc7b501971342452591741c2173560d5df823f4
ko9612 <kohana735@gmail.com>
2022-11-30 17:37:18 +0900

    top list 테스트

--- client/src/components/main/DailyPost.jsx
+++ client/src/components/main/DailyPost.jsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { useInView } from 'react-intersection-observer';
 import DailyInfomation from './DailyInfo';
 import DailyImg from './DailyImg';
-import { DailyForm, DailyItem } from './MainStyle';
+import dailyAdd from '../../images/daily_add.svg';
+import { DailyForm, DailyItem, Top, Content } from './MainStyle';
 import Loader from './Loader';
 
 export default function DailyPost() {
   const [postList, setPostList] = useState([]);
-
+  console.log(postList);
   const lastPost = postList && postList[postList.length - 1];
   const [isLoaded, setIsLoaded] = useState(false);
 
@@ -18,6 +20,15 @@ export default function DailyPost() {
   //   console.log(filteredCmt);
   //   setBol(false);
   // };
+
+  const navigate = useNavigate();
+  const newPost = () => {
+    // if (!ac) {
+    //   alert('로그인 후 이용할 수 있습니다.');
+    // } else {
+    navigate('/dailypost');
+    // }
+  };
 
   const [ref, inView] = useInView();
 
@@ -44,25 +55,46 @@ export default function DailyPost() {
   }, [inView]);
 
   return (
-    <DailyForm>
-      {postList &&
-        postList.map(all => {
-          return (
-            <div className="list">
-              {all &&
-                all.map(list => {
-                  return (
-                    <DailyItem key={list.post.postId}>
-                      <DailyImg el={list} />
-                      <DailyInfomation el={list} index={list.post.postId} />
-                    </DailyItem>
-                  );
-                })}
-              <div ref={ref} />
-            </div>
-          );
-        })}
-      <div ref={ref}>{isLoaded && <Loader />}</div>
-    </DailyForm>
+    <>
+      <Top>
+        {postList[0] &&
+          postList[0].map(el => {
+            return (
+              <Content>
+                <div className="imgprofile">
+                  <img src={el.member.profileImage} alt="userProfile" />
+                </div>
+                <span className="userName">{el.member.username}</span>
+              </Content>
+            );
+          })}
+        <Content>
+          <button className="imgprofile dailynew" onClick={() => newPost()}>
+            <img src={dailyAdd} alt="dailyAdd" />
+          </button>
+          <span>새 게시물</span>
+        </Content>
+      </Top>
+      <DailyForm>
+        {postList &&
+          postList.map(all => {
+            return (
+              <div className="list">
+                {all &&
+                  all.map(list => {
+                    return (
+                      <DailyItem key={list.post.postId}>
+                        <DailyImg el={list} />
+                        <DailyInfomation el={list} index={list.post.postId} />
+                      </DailyItem>
+                    );
+                  })}
+                <div ref={ref} />
+              </div>
+            );
+          })}
+        <div ref={ref}>{isLoaded && <Loader />}</div>
+      </DailyForm>
+    </>
   );
 }
