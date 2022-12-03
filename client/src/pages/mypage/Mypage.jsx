f4b36836e1a30d941ce9940b5223085191f5edf5
Helperbot5 <eswirdtheo@gmail.com>
2022-12-03 14:51:18 +0900

    merge: 병합

--- client/src/pages/mypage/Mypage.jsx
+++ client/src/pages/mypage/Mypage.jsx
@@ -81,7 +81,7 @@ function Mypage() {
             </div>
             <div className="box">
               <div>Point</div>
-              {activity.point ? activity.point : 0}점
+              {Math.round(activity.point) ? Math.round(activity.point) : 0}점
             </div>
           </div>
           <button className="editBtn" onClick={() => navigate('/mypage/edit')}>
