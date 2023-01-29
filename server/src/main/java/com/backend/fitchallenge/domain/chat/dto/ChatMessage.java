1712d0cc911f388965df55fbca87af1b600368e2
Inssg <nightwalker0131@naver.com>
2023-01-29 18:07:47 +0900

    feat: ChatMessage Dto 추가

---
+++ server/src/main/java/com/backend/fitchallenge/domain/chat/dto/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.backend.fitchallenge.domain.chat.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ChatMessage {
+
+    private MessageType type;
+    private String roomId;
+    private String sender; //메시지 보낸사람
+    private String message;
+
+    // 메시지 타입 : 입장, 채팅
+    public enum MessageType {
+        ENTER, TALK
+    }
+}
