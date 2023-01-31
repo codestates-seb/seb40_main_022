03124624020aa8209d539623323903f1b74a729a
Inssg <nightwalker0131@naver.com>
2023-01-31 22:23:34 +0900

    fix: RedisSubscriber 수정

--- server/src/main/java/com/backend/fitchallenge/global/redis/chat/RedisSubscriber.java
+++ server/src/main/java/com/backend/fitchallenge/global/redis/chat/RedisSubscriber.java
@@ -12,9 +12,11 @@ import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Service;
 
 @Slf4j
 @RequiredArgsConstructor
+@Service
 public class RedisSubscriber implements MessageListener {
     private final ObjectMapper objectMapper;
     private final RedisTemplate redisTemplate;
